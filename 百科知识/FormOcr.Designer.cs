3213adeaad7b3fd930b86b2efd62db9b754315c6
吴福顺 <wufushun@users.noreply.github.com>
2019-12-19 16:40:46 +0800

    Add files via upload
    入库

---
+++ 百科知识/FormOcr.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace 百科知识
+{
+    partial class FormOcr
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOcr));
+            this.openFileDlg1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOcr = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnHandOcr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDlg1
+            // 
+            this.openFileDlg1.FileName = "openFileDialog1";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(361, 12);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 33);
+            this.btnFile.TabIndex = 0;
+            this.btnFile.Text = "选择文件";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 453);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // btnOcr
+            // 
+            this.btnOcr.Location = new System.Drawing.Point(361, 51);
+            this.btnOcr.Name = "btnOcr";
+            this.btnOcr.Size = new System.Drawing.Size(75, 33);
+            this.btnOcr.TabIndex = 2;
+            this.btnOcr.Text = "提取";
+            this.btnOcr.UseVisualStyleBackColor = true;
+            this.btnOcr.Click += new System.EventHandler(this.btnOcr_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(442, 9);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(346, 453);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnHandOcr
+            // 
+            this.btnHandOcr.Location = new System.Drawing.Point(361, 90);
+            this.btnHandOcr.Name = "btnHandOcr";
+            this.btnHandOcr.Size = new System.Drawing.Size(75, 33);
+            this.btnHandOcr.TabIndex = 4;
+            this.btnHandOcr.Text = "手写提取";
+            this.btnHandOcr.UseVisualStyleBackColor = true;
+            this.btnHandOcr.Click += new System.EventHandler(this.btnHandOcr_Click);
+            // 
+            // FormOcr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.btnHandOcr);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOcr);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFile);
+            this.Name = "FormOcr";
+            this.Text = "图片文字提取";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDlg1;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOcr;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnHandOcr;
+    }
+}
+
---
+++ 百科知识/FormOcr.cs
@@ -0,0 +1,105 @@
+﻿using Baidu.Aip.Ocr;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 百科知识
+{
+    public partial class FormOcr : Form
+    {
+        // 设置APPID/AK/SK
+        string APP_ID = "18045209";
+        string API_KEY = "bgEo5c5L25OqR3O87RDXXx0C";
+        string SECRET_KEY = "rGD4muljRZnVGQMIEKjGrLY0HO90LqmF";
+        Ocr client;
+
+        public FormOcr()
+        {
+            InitializeComponent();
+            client = new Baidu.Aip.Ocr.Ocr(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDlg1.ShowDialog();
+            string fileName = openFileDlg1.FileName;
+            if (dr == System.Windows.Forms.DialogResult.OK && !string.IsNullOrEmpty(fileName))
+            {
+                try
+                {
+                    pictureBox1.Load(fileName);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private void btnOcr_Click(object sender, EventArgs e)
+        {
+
+            Ocr(openFileDlg1.FileName, "", null);
+        }
+
+        private void btnHandOcr_Click(object sender, EventArgs e)
+        {
+            Ocr(openFileDlg1.FileName, "hand",null);
+        }
+
+        private void Ocr(string fileName, string type, Dictionary<string, object> options)
+        {
+            if (String.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+            // 调用通用文字识别, 图片参数为本地图片，可能会抛出网络等异常，请使用try/catch捕获
+            try
+            {
+                // 带参数调用通用文字识别, 图片参数为本地图片,options可选
+                JToken result;
+                byte[] image = File.ReadAllBytes(fileName);
+                switch (type) {
+                    case "hand":
+                        if (null == options)
+                        {
+                            result = client.Handwriting(image);
+                        }
+                        else
+                        {
+                            result = client.Handwriting(image, options);
+                        }
+                        break;
+
+                    default :
+                        result = client.GeneralBasic(image);
+                        break;
+                }
+                image = null;
+                 
+                JArray wordsArray = (JArray)result["words_result"];
+                string wordsStr = "";
+                for (int i = 0; i < wordsArray.Count; i++)
+                {
+                    wordsStr += wordsArray[i]["words"] + "\r\n";
+                }
+                textBox1.Text = wordsStr;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+
+}
